807b946f2f7081240ba8315ce2b51d5190f885e3
garfild <garfild13-06@yandex.com>
2024-08-01 15:25:22 +0300

    Init Commit

---
+++ src/js/Character.js
@@ -0,0 +1,22 @@
+class Character {
+    constructor(name, type) {
+        const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+
+        if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+            throw new Error('Имя должно быть строкой от 2 до 10 символов');
+        }
+
+        if (!types.includes(type)) {
+            throw new Error('Некорректный тип персонажа');
+        }
+
+        this.name = name;
+        this.type = type;
+        this.health = 100;
+        this.level = 1;
+        this.attack = 0;
+        this.defence = 0;
+    }
+}
+
+module.exports = Character;
